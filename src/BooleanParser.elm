afd459c9dae1e55fd97fc5e4bf87541c5da2e073
m-shaka <buddha0818@gmail.com>
2018-02-04 21:29:17 +0900

    first commit

---
+++ src/BooleanParser.elm
@@ -0,0 +1,98 @@
+module BooleanParser exposing (parseString, evalString)
+
+import Parser exposing (..)
+
+
+type Term
+    = T
+    | F
+    | Not Term
+    | And Term Term
+    | Or Term Term
+
+
+term : Parser Term
+term =
+    let
+        true =
+            succeed T |. keyword "T"
+
+        false =
+            succeed F |. keyword "F"
+
+        bracket =
+            succeed identity
+                |. symbol "("
+                |. spaces
+                |= lazy (\_ -> term)
+                |. spaces
+                |. symbol ")"
+
+        not_ =
+            succeed Not
+                |. keyword "not"
+                |. spaces
+                |= oneOf [ true, false, lazy (\_ -> not_), bracket ]
+    in
+        succeed identity
+            |. spaces
+            |= oneOf
+                [ bracket, true, false, not_ ]
+            |. spaces
+            |> andThen
+                (\t ->
+                    oneOf
+                        [ succeed (And t)
+                            |. symbol "&&"
+                            |. spaces
+                            |= lazy (\_ -> term)
+                        , succeed (Or t)
+                            |. symbol "||"
+                            |. spaces
+                            |= lazy (\_ -> term)
+                        , succeed t
+                            |. spaces
+                        ]
+                )
+
+
+spaces : Parser ()
+spaces =
+    ignore zeroOrMore (\char -> char == ' ')
+
+
+parseString : String -> Result Error Term
+parseString s =
+    run term s
+
+
+evalTerm : Term -> Bool
+evalTerm term =
+    case term of
+        T ->
+            True
+
+        F ->
+            False
+
+        Not t ->
+            not (evalTerm t)
+
+        And t1 t2 ->
+            (evalTerm t1) && (evalTerm t2)
+
+        Or t1 t2 ->
+            (evalTerm t1) || (evalTerm t2)
+
+
+evalString : String -> String
+evalString s =
+    case parseString s of
+        Ok value ->
+            if evalTerm value then
+                "True"
+            else
+                "False"
+
+        Err err ->
+            "Parse Error"
---
+++ src/Main.elm
@@ -0,0 +1,87 @@
+module Main exposing (..)
+
+import Html exposing (..)
+import Html.Events exposing (..)
+import Html.Attributes exposing (..)
+import BooleanParser exposing (evalString)
+
+
+main =
+    beginnerProgram
+        { view = view
+        , model = model
+        , update = update
+        }
+
+
+type alias Model =
+    { input_ : String, result : String }
+
+
+type Msg
+    = Change_input String
+    | Submit
+
+
+model : Model
+model =
+    { input_ = "", result = "" }
+
+
+update : Msg -> Model -> Model
+update msg model =
+    case msg of
+        Change_input s ->
+            { model | input_ = s }
+
+        Submit ->
+            { model | result = evalString model.input_ }
+
+
+descriptions : List (Html Msg)
+descriptions =
+    let
+        convert s =
+            String.split "\n" s |> List.map String.trim |> String.join "" |> text
+    in
+        [ """
+          論理演算をするだけのパーサー。Elmで実装。True, Falseはそれぞれ'T', 'F'、否定は'not'、
+          積と和はそれぞれ'&&', '||'に対応している。
+          括弧付きの表記にも対応している。
+          """
+        ]
+            |> List.map convert
+
+
+examples =
+    ul []
+        [ li [] [ text "not T \\(\\rightarrow \\neg{True} \\Rightarrow False\\)" ]
+        , li [] [ text "T && T \\(\\rightarrow True \\land True \\Rightarrow True \\)" ]
+        , li [] [ text "not (T || F) && T \\(\\rightarrow \\neg{(True \\lor False)} \\land True \\Rightarrow False \\)" ]
+        ]
+
+
+view : Model -> Html Msg
+view model =
+    let
+        inputStyle =
+            style [ ( "font-size", "20px" ) ]
+
+        buttonStyle =
+            style [ ( "margin", "auto 20px" ) ]
+    in
+        div
+            [ style
+                [ ( "margin", "100px auto" )
+                , ( "max-width", "800px" )
+                ]
+            ]
+            [ Html.form [ onSubmit Submit ]
+                [ input [ onInput Change_input, type_ "text", value model.input_, inputStyle ] []
+                , button [ type_ "submit", buttonStyle ] [ text "評価" ]
+                , text model.result
+                ]
+            , p [] descriptions
+            , h4 [] [ text "examples" ]
+            , examples
+            ]
